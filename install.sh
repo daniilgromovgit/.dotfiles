0bc5848145dbb0b87a90cfbdf64abb412fac95fc
daniil <243645856+daniilgromovgit@users.noreply.github.com>
2026-07-08 15:12:33 +0300

    Fix stow conflicts during install

--- install.sh
+++ install.sh
@@ -10,11 +10,16 @@ sudo pacman -S --needed $(grep -v '^#' "$DOTFILES/packages.txt")
 echo "Setting zsh..."
 chsh -s "$(which zsh)"
 
-echo "Deploying configs..."
+echo "Removing existing configs..."
+rm -rf ~/.zshrc
+rm -rf ~/.config/kitty
+rm -rf ~/.config/niri
+rm -f ~/.config/starship.toml
+
+echo "Deploying dotfiles..."
 cd "$DOTFILES"
 
 stow zsh
 stow config
 
-echo "Finished!"
-echo "Restart your session."
+echo "Done! Restart your session."
